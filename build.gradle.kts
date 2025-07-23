c4a7437d26fd0e87ed1729b7ca5997508caecb3e
MarianKijewski <mariankijewski@outlook.com>
2025-07-23 09:17:07 +0200

    add Ktlint and Detekt plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,44 @@
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+
 plugins {
     kotlin("jvm") version "2.1.21"
+    id("org.jlleitschuh.gradle.ktlint") version "12.3.0"
+    id("io.gitlab.arturbosch.detekt") version "1.23.8"
+    application
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "com.wire"
+version = "1.0"
 
 repositories {
     mavenCentral()
 }
+application {
+    mainClass.set("com.wire.broadcastapp.ApplicationKt")
+}
 
 dependencies {
     testImplementation(kotlin("test"))
+}
+
+ktlint {
+    verbose.set(true)
+    outputToConsole.set(true)
+    coloredOutput.set(true)
+    reporters {
+        reporter(ReporterType.CHECKSTYLE)
+        reporter(ReporterType.JSON)
+        reporter(ReporterType.HTML)
+    }
+}
+
+detekt {
+    toolVersion = "1.23.7"
+    config.setFrom(file("$rootDir/config/detekt/detekt.yml"))
+    baseline = file("$rootDir/config/detekt/baseline.xml")
+    parallel = true
+    buildUponDefaultConfig = true
+    source.setFrom("src/main/kotlin")
 }
 
 tasks.test {
---
+++ src/main/kotlin/com/wire/broadcastapp/Application.kt
@@ -0,0 +1,5 @@
+package com.wire.broadcastapp
+
+fun main() {
+    println("Hello Wire!")
+}

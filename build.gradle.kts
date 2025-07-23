c173da65347be134390103d4436c0309fc8ba1bf
MarianKijewski <148276531+MarianKijewski@users.noreply.github.com>
2025-07-23 12:39:57 +0200

    Chore/setup gradle project with development dependencies
    * generate Gradle project
    
    * add CODEOWNERS
    
    * add Ktlint and Detekt plugins
    
    * add CI and dependabot

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+
+plugins {
+    kotlin("jvm") version "2.1.21"
+    id("org.jlleitschuh.gradle.ktlint") version "12.3.0"
+    id("io.gitlab.arturbosch.detekt") version "1.23.8"
+    application
+}
+
+group = "com.wire"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+application {
+    mainClass.set("com.wire.broadcastapp.ApplicationKt")
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+ktlint {
+    verbose.set(true)
+    outputToConsole.set(true)
+    coloredOutput.set(true)
+    reporters {
+        reporter(ReporterType.CHECKSTYLE)
+        reporter(ReporterType.JSON)
+        reporter(ReporterType.HTML)
+    }
+}
+
+detekt {
+    toolVersion = "1.23.8"
+    config.setFrom(file("$rootDir/config/detekt/detekt.yml"))
+    baseline = file("$rootDir/config/detekt/baseline.xml")
+    parallel = true
+    buildUponDefaultConfig = true
+    source.setFrom("src/main/kotlin")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}

e20da87b7f88c8ba053633167682fb21c3a5592f
MarianKijewski <148276531+MarianKijewski@users.noreply.github.com>
2026-01-19 09:57:10 +0100

    add welcome message (#23)
    * add welcome message
    
    * fix emoji appearing as string representation

--- src/main/kotlin/com/wire/broadcastapp/EventsHandler.kt
+++ src/main/kotlin/com/wire/broadcastapp/EventsHandler.kt
@@ -1,7 +1,10 @@
 package com.wire.broadcastapp
 
+import com.wire.broadcastapp.BroadcastService.Companion.COMMAND_PREFIX
 import com.wire.broadcastapp.dao.Repository
 import com.wire.sdk.WireEventsHandlerSuspending
+import com.wire.sdk.model.ConversationData
+import com.wire.sdk.model.ConversationMember
 import com.wire.sdk.model.WireMessage
 
 class EventsHandler : WireEventsHandlerSuspending() {
@@ -11,5 +14,25 @@ class EventsHandler : WireEventsHandlerSuspending() {
 
     override suspend fun onTextMessageReceived(wireMessage: WireMessage.Text) {
         broadcast.handleMessage(wireMessage)
+    }
+
+    override suspend fun onAppAddedToConversation(
+        conversation: ConversationData,
+        members: List<ConversationMember>
+    ) {
+        val welcomeMessage = WireMessage.Text.create(
+            conversationId = conversation.id,
+            text = WELCOME_TEXT
+        )
+
+        manager.sendMessage(welcomeMessage)
+    }
+
+    private companion object {
+        const val WELCOME_TEXT =
+            "👋 Hi, I'm the Broadcast App. Thanks for adding me to the conversation.\n" +
+                "You can use me to message multiple conversations simultaneously.\n" +
+                "I'm here to help make everyday work a little easier.\n" +
+                "Use the `$COMMAND_PREFIX <your message here>` command to get started."
     }
 }
